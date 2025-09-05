123259fe97b60f019ffa171f177080d0813b1924
juanchisnyc-art <juanchisnyc@gmail.com>
2025-09-05 04:52:24 +0000

    finish class example

---
+++ lab2_operators/lab2.cpp
@@ -0,0 +1,67 @@
+/*
+Juan Velasco
+Lab 2, operators
+Sep 2,2025
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\n----- example 1: arithmetic operator ----" <<endl;
+    //set number format
+    cout.setf(ios::fixed);
+    cout.precision(3);
+// calculate the hieght of a free falling object
+// free_fall = 0.5*gravity*time*time
+// declare a constant variable named 'gravity'
+    const float GRAVITY = 9.8;
+// dclare variable height and time
+float height=0 , time=0;
+// collect the falling time
+    cout<<"Enter a falling time";
+    cin>>time;
+// calculte the height of a free fall object
+height = 0.5*GRAVITY*time*time;
+//print results
+cout<< "The higeht of a free fall object at "<<time<<" second is"<< height<<endl;
+ 
+cout<<"\n---- example 2: assigntment operator----"<<endl;
+int number1 = 10;
+int number2 = 3;
+cout<<"original number1 = "<<number1<<endl;
+number1 ++;
+cout<<"updated number1 = "<<number1<<endl;
+cout<<"original number2 = "<<number2<<endl;
+number2--;
+cout<<"updated number2 = "<<number2<<endl;
+number2 += 5;
+cout<<"original number2 by 5 = "<<number2<<endl;
+number1 *= 2;
+cout<<"updated number1 using *=2 --> = "<<number1<<endl;
+
+cout<<"the remainder of "<<number1<<"and"<<number2<<"is"<<number1%number2<<endl;
+cout<<"after %=,number1 ="<<number1<< endl;
+cout<<"after %=,number2 ="<<number2<<endl;
+
+number1 %= number2;
+cout<<"after %=, number1 = "<<number1<<endl;
+cout<<"affter %=, number2 = "<<number2<<endl;
+
+cout<<"\n---- example 3: comparison operator -----"<<endl;
+cout<<"is number1 equal to number 2?"<<(number1==2)<<endl;
+cout<<"is number1 greater than number2?"<<(number1>number2)<<endl;
+cout<<"is number1 less than  number2? "<<(number1< number1)<<endl;
+
+
+cout<<"n----- example 4: logical operator ---"<<endl;
+// AND operator
+bool check_numbers = (number1 != number2) && (number1 < number2) && (number1>5);
+// number 1 != number2 --> true
+// number 1 < number2 --> true
+// number1>5 --> false
+//check_numbers = true && true && false = false
+cout<<"The result of check_number is "<< check_numbers<<endl;
+
+return 0;
+}
