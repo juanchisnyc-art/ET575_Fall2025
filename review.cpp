af4557d1106f3ee3733c5cd9d987f2bc35bc7936
juanchisnyc-art <juanchisnyc@gmail.com>
2025-10-08 19:21:50 +0000

    Lab 8 finish hw

---
+++ review.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+ int age;
+ cout<<"Enter your age:" ;
+ cin>> age;
+
+ if (age >=21){
+    cout<<"You can vote";
+ }else{
+        cout<< 21 
+    }
+ }
+
+    return 0 
+}
+
+
+
