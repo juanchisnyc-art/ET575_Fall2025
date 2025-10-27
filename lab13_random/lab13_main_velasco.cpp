db8f11c9dd5b37391184df31d8a359fc21905d04
juanchisnyc-art <juanchisnyc@gmail.com>
2025-10-27 19:26:28 +0000

    save

--- lab13_random/lab13_main_velasco.cpp
+++ lab13_random/lab13_main_velasco.cpp
@@ -18,45 +18,22 @@ int main(){
     int diff_random = different_random();
     cout<<"Differeent random number:"<<diff_random<<endl;
 
-
-
-
     cout<<"\n ---- Example 3: random number between 0 and 9 ----- "<<endl;
     int r_0_9 = random_0_9();
     cout<<"Number between 0 and 9: \t"<<r_0_9<<endl;
 
-        cout<<"\n ---- Example 4: random number between -10 and 5 ----- "<<endl;
-        cout<<"Number between -10 and 5 \t "<<random_neg10_5()<<endl;
+    cout<<"\n ---- Example 4: random number between -10 and 5 ----- "<<endl;
+    cout<<"Number between -10 and 5 \t "<<random_neg10_5()<<endl;
 
-        cout<<"\n ----- EXCERCISE ------ "<<endl;
-        int run1 = random_1_6();
-        int run2 = random_1_6();
+    cout<<"\n ----- EXCERCISE ------ "<<endl;
+    int run1 = random_1_6();
+    int run2 = random_1_6();
 
-        // calll function to check for snake_eyes
-        bool result = compare_ones(run1,run2);
+    // call function to check for snake_eyes
+    bool result = compare_ones(run1, run2);
 
-        // call function to print result
-        printresult(result);
-   // exercise: a die roll 1..6 (inclusive)
-int random_1_6(){
-    ensureSeeded();
-    return (rand() % 6) + 1;            // 1..6
-}
+    // call function to print result
+    printresult(result);
 
-// exercise: check for snake eyes (both dice are 1)
-bool compare_ones(int a, int b) {
-    return (a == 1 && b == 1);
-}
-
-// exercise: print the result message based on compare_ones()
-void printresult(bool is_snake_eyes) {
-    if (is_snake_eyes) {
-        cout << "Snake eyes! You rolled double ones!" << endl;
-    } else {
-        cout << "Not snake eyes." << endl;
-    }
-}
-   
-   
-        return 0;
+    return 0;
 }
